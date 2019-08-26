64ec592c8855d4650f1988acc99216a29a8e9666
imed jaberi <imed_jebari@hotmail.fr>
2019-08-26 03:33:13 +0200

    add redux-devtools-extension as comment for future / discover ..

--- src/store/Custom Store.js
+++ src/store/Custom Store.js
@@ -1,11 +1,15 @@
 // import redux dep... 
 const { createStore } = require('redux');
 
+// import redx-devtools-extension for chrome | fireforx ..
+// const { composeWithDevTools } = require('redux-devtools-extension');
+
+
 // import the reducer ..  
 const Custom_Reducer = require('../reducers/Custom Reducer');
 
 // create the store .. 
-const Custom_Store = createStore(Custom_Reducer);
+const Custom_Store = createStore(Custom_Reducer/*, composeWithDevTools()*/);
 
 // export the store .. 
 module.exports = Custom_Store ;
